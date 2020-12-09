eef4c176805abb24bea8329ee244eaa2a19470ac
Alexly <podzemliu@gmail.com>
2020-12-09 18:13:43 +0200

    migrations successfully

--- booking/admin.py
+++ booking/admin.py
@@ -6,7 +6,7 @@ from .models import Event, Added
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     # todo: добавить поле "количество присоединившихся групп". Title="Присоединилось" и метод для подсчета количества связанных объектов Added
-    # todo: добавить поле и метод "всего участников в событии". Title="Всего участников"
+    # todo: добавить поле "всего участников в событии". Title="Всего участников" с соответствующим методом
     list_display = ('type', 'start_datetime', 'initiator_name', 'initiator_phone',
                     'initiator_email', 'initiator_istelegram', 'users_number',
                     'created_datetime', 'allow_join', 'approved', 'done', 'annulled',)
